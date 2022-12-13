022ae17b181b92aa17febeb9341247d276c092c7
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-12-13 22:01:07 +0300

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Anton Boev on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
